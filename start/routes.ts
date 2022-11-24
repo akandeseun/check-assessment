0947413f58d1da7c7118cb3ef826eae6ad9d43c8
akandeseun <akandeseun44@gmail.com>
2022-11-24 21:35:41 +0100

    lending migrations

--- start/routes.ts
+++ start/routes.ts
@@ -22,3 +22,4 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/register', 'AuthController.register')
 Route.post('/sign-in', 'AuthController.authenticate')
+Route.post('/update', 'UserController.updateProfile').middleware('auth')
