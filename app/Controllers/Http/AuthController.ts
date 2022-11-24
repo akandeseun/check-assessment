23c7a12cc45ed1a99e5d5f99a62963db9fe664d8
akandeseun <akandeseun44@gmail.com>
2022-11-24 12:10:39 +0100

    authenticate logic

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Hash from '@ioc:Adonis/Core/Hash'
 import CreateUserValidator from 'App/Validators/CreateUserValidator'
 import AuthenticateUserValidator from 'App/Validators/AuthenticateUserValidator'
 import User from 'App/Models/User'
@@ -13,5 +14,28 @@ export default class AuthController {
 
   public async authenticate({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(AuthenticateUserValidator)
+
+    try {
+      const user = await User.findBy('email', payload.email)
+      if (!user) {
+        return response.status(400).json({
+          msg: 'Incorrect email or password',
+        })
+      }
+
+      if (!(await Hash.verify(user.password, payload.password))) {
+        return response.status(400).json({
+          msg: 'Incorrect email or password',
+        })
+      }
+
+      const token = await auth.use('api').generate(user, { expiresIn: '30 days' })
+      return response.status(200).json({
+        msg: `welcome ${user.username}`,
+        token,
+      })
+    } catch (error) {
+      return response.unauthorized('error')
+    }
   }
 }
