d561c0b644bbf9b033bc854554ebc0987d39b8fb
akandeseun <akandeseun44@gmail.com>
2022-11-24 12:38:42 +0100

    update profile validator

--- app/Validators/UpdateProfileValidator.ts
+++ app/Validators/UpdateProfileValidator.ts
@@ -1,40 +1,18 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UpdateProfileValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+  public schema = schema.create({
+    username: schema.string.nullableAndOptional({ trim: true }, [
+      rules.unique({ table: 'users', column: 'username' }),
+    ]),
+    first_name: schema.string.nullableAndOptional(),
+    last_name: schema.string.nullableAndOptional(),
+    age: schema.number.nullableAndOptional(),
+    address: schema.string.nullableAndOptional(),
+  })
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
   public messages: CustomMessages = {}
 }
