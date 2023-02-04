87cae9d347856afa68d26b067908f658bd53a8d9
akandeseun <akandeseun44@gmail.com>
2023-02-04 15:55:01 +0100

    w.i.p: models

--- app/Models/Profile.ts
+++ app/Models/Profile.ts
@@ -3,7 +3,19 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Profile extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string
+
+  @column()
+  public firstName: string
+
+  @column()
+  public lastName: string
+
+  @column()
+  public age: number
+
+  @column()
+  public address: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
